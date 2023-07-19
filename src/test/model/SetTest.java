b632893169cf9a62b3aea0a7644c103935f08683
ericbalanecki <ebalanecki@gmail.com>
2023-07-19 01:26:11 -0700

    Set up branch coverage checker

--- src/test/model/SetTest.java
+++ src/test/model/SetTest.java
@@ -7,15 +7,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class SetTest {
     private Set testSet;
+    private Set testSet2;
 
     @BeforeEach
     void runBefore() {
         testSet = new Set(12, 70);
+        testSet2 = new Set(10, 80);
     }
 
     @Test
     void testConstructor() {
         assertEquals(12, testSet.getReps());
         assertEquals(70, testSet.getPercent1RM());
+
+        assertEquals(10, testSet2.getReps());
+        assertEquals(80, testSet2.getPercent1RM());
     }
 }
